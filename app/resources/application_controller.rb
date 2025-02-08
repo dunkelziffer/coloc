6ba592e03e156dcc722a16787350d2910ada0052
Klaus Weidinger <weidkl@gmx.de>
2025-02-08 23:06:15 +0100

    Move controllers to common resource folder

--- app/resources/application_controller.rb
+++ app/resources/application_controller.rb
@@ -1,4 +1,8 @@
 class ApplicationController < ActionController::Base
   # Only allow modern browsers supporting webp images, web push, badges, import maps, CSS nesting, and CSS :has.
   allow_browser versions: :modern
+
+  def self.controller_path
+    super&.delete_suffix('/')
+  end
 end
--- app/resources/tasks/controller.rb
+++ app/resources/tasks/controller.rb
@@ -1,4 +1,4 @@
-class TasksController < ApplicationController
+class Tasks::Controller < ApplicationController
 
   def index
     load_tasks
