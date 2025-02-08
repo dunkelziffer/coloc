712c3e38098c18e94fbccf455c2c4239f94c6dba
Klaus Weidinger <weidkl@gmx.de>
2025-02-08 22:22:27 +0100

    Refactor controller

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -1,70 +1,75 @@
 class TasksController < ApplicationController
-  before_action :set_task, only: %i[ show edit update destroy ]
 
-  # GET /tasks or /tasks.json
   def index
-    @tasks = Task.all
+    load_tasks
   end
 
-  # GET /tasks/1 or /tasks/1.json
   def show
+    load_task
   end
 
-  # GET /tasks/new
   def new
-    @task = Task.new
+    build_task
   end
 
-  # GET /tasks/1/edit
-  def edit
-  end
-
-  # POST /tasks or /tasks.json
   def create
-    @task = Task.new(task_params)
+    build_task
 
-    respond_to do |format|
-      if @task.save
-        format.html { redirect_to @task, notice: "Task was successfully created." }
-        format.json { render :show, status: :created, location: @task }
-      else
-        format.html { render :new, status: :unprocessable_entity }
-        format.json { render json: @task.errors, status: :unprocessable_entity }
-      end
-    end
+    if @task.save
+      flash[:notice] = 'Task was successfully created'
+      redirect_to @task
+    else
+      render 'new', status: :unprocessable_entity
+    end  
   end
 
-  # PATCH/PUT /tasks/1 or /tasks/1.json
+  def edit
+    load_task
+    build_task
+  end
+
   def update
-    respond_to do |format|
-      if @task.update(task_params)
-        format.html { redirect_to @task, notice: "Task was successfully updated." }
-        format.json { render :show, status: :ok, location: @task }
-      else
-        format.html { render :edit, status: :unprocessable_entity }
-        format.json { render json: @task.errors, status: :unprocessable_entity }
-      end
+    load_task
+    build_task
+
+    if @task.save
+      flash[:notice] = 'Task was successfully updated'
+      redirect_to @task
+    else
+      render 'edit', status: :unprocessable_entity
     end
   end
 
-  # DELETE /tasks/1 or /tasks/1.json
   def destroy
+    load_task    
+    
     @task.destroy!
-
-    respond_to do |format|
-      format.html { redirect_to tasks_path, status: :see_other, notice: "Task was successfully destroyed." }
-      format.json { head :no_content }
-    end
+    flash[:notice] = 'Task was successfully destroyed'
+    redirect_to :tasks
   end
 
   private
-    # Use callbacks to share common setup or constraints between actions.
-    def set_task
-      @task = Task.find(params.expect(:id))
-    end
 
-    # Only allow a list of trusted parameters through.
-    def task_params
-      params.expect(task: [ :description, :done ])
-    end
+  def load_tasks
+    @tasks ||= task_scope.strict_loading.to_a
+  end
+
+  def load_task
+    @task ||= task_scope.find(params[:id])
+  end
+
+  def build_task
+    @task ||= task_scope.build
+    @task.attributes = task_params
+  end
+
+  def task_params
+    params.require(:task).permit(:description, :done)
+  rescue ActionController::ParameterMissing  
+    {}
+  end
+
+  def task_scope
+    Task.all
+  end
 end
